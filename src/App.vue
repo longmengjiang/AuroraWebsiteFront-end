7a94bd19cfbcd21faedb99415734a9355a6c67ef
longmengjiang <1255086223@qq.com>
2024-08-27 19:38:25 +0800

    调整错误提交，与上一次提交并无区别

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="app">
+    <!-- 远程仓库别名   AuroraWebsiteFront-end  -->
     <NavBar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import NavBar from "./components/aNavBar.vue"
+import NavBar from "./components/aNavBar.vue";
 export default {
   components: {
-    NavBar
+    NavBar,
   },
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
